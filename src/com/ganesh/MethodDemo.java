43b826d14fc723455d22badf56e59a5bd11a34a8
Ganesh6328 <ganeshshinde5492@gmail.com>
2023-09-28 16:35:52 +0530

    first method demo program

---
+++ src/com/ganesh/MethodDemo.java
@@ -0,0 +1,19 @@
+package com.ganesh;
+
+public class MethodDemo {
+	
+	public static void method()
+	{
+		System.out.println("Method started!!!!");
+		System.out.println("Method ended!!!!");
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Main method started!!!!");
+		System.out.println("Main method ended!!!!");
+
+	}
+
+}
+
