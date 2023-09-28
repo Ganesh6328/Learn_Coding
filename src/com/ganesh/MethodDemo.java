a6cae5a198a1b70f7b13003a983f62bd8c783ef6
Ganesh6328 <ganeshshinde5492@gmail.com>
2023-09-28 16:41:32 +0530

    add another method in same program

--- src/com/ganesh/MethodDemo.java
+++ src/com/ganesh/MethodDemo.java
@@ -6,11 +6,19 @@ public class MethodDemo {
 	{
 		System.out.println("Method started!!!!");
 		System.out.println("Method ended!!!!");
-		
+	}
+	
+	public void method1()
+	{
+		System.out.println("Method1 started!!!");
+		System.out.println("Method1 ended!!!");
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Main method started!!!!");
+		method();
+		MethodDemo obj = new MethodDemo();
+		obj.method1();
 		System.out.println("Main method ended!!!!");
 
 	}
